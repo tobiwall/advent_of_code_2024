401666366913d3d4b519c2f40890622748879430
tobiwall <t.w.95@web.de>
2024-12-11 11:12:31 +0100

    day 7 task1 and task2

---
+++ day7/src/main.rs
@@ -0,0 +1,103 @@
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    let path = "input.txt";
+    let file = File::open(path).expect("Failed to open the file");
+    let reader = BufReader::new(file);
+    let lines = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+
+    let mut count = 0;
+    let mut count2 = 0;
+    for line in lines {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let result = parts[0].parse::<i64>().expect("Failed to parse result");
+        let numbers_str: Vec<&str> = parts[1].split_whitespace().collect();
+        let mut numbers = Vec::new();
+        let mut line_map: HashMap<i64, Vec<i32>> = HashMap::new();
+        for number in numbers_str {
+            numbers.push(number.parse::<i32>().unwrap());
+        }
+        line_map.insert(result, numbers);
+
+        for (key, numbers) in &line_map {
+            let result = find_combination(numbers, *key);
+            if result == true {
+                count += key;
+                count2 += key;
+            } else {
+                let result = find_combination_with_concat(numbers, *key);
+                if result == true {
+                    count2 += key;
+                }
+            }
+        }
+    }
+    println!("Task 1: {count:?}");
+    println!("Task 2: {count2:?}");
+}
+
+fn find_combination(numbers: &[i32], key: i64) -> bool {
+    fn evaluate_expressions(numbers: &[i32], idx: usize, current_value: i64) -> Vec<i64> {
+        if idx == numbers.len() {
+            return vec![current_value];
+        }
+
+        let mut results = Vec::new();
+
+        results.extend(evaluate_expressions(
+            numbers,
+            idx + 1,
+            current_value + numbers[idx] as i64,
+        ));
+
+        results.extend(evaluate_expressions(
+            numbers,
+            idx + 1,
+            current_value * numbers[idx] as i64,
+        ));
+
+        results
+    }
+    let possible_results = evaluate_expressions(numbers, 1, numbers[0] as i64);
+    possible_results.contains(&key)
+}
+
+fn find_combination_with_concat(numbers: &[i32], key: i64) -> bool {
+    fn evaluate_expressions(numbers: &[i32], idx: usize, current_value: i64) -> Vec<i64> {
+        if idx == numbers.len() {
+            return vec![current_value];
+        }
+
+        let mut results = Vec::new();
+
+        results.extend(evaluate_expressions(
+            numbers,
+            idx + 1,
+            current_value + numbers[idx] as i64,
+        ));
+
+        results.extend(evaluate_expressions(
+            numbers,
+            idx + 1,
+            current_value * numbers[idx] as i64,
+        ));
+
+        let combined_value = format!("{}{}", current_value, numbers[idx])
+            .parse::<i64>()
+            .unwrap_or(current_value);
+        results.extend(evaluate_expressions(numbers, idx + 1, combined_value));
+
+        results
+    }
+
+    let possible_results = evaluate_expressions(numbers, 1, numbers[0] as i64);
+
+    possible_results.contains(&key)
+}
